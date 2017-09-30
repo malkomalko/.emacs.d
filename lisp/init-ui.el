51190ba22d44eb0d79242f8e32fc155ffd599b83
Robert Malko <rmalko@doximity.com>
2017-09-30 13:00:27 -0700

    add gruvbox color theme

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -1,6 +1,12 @@
+; turn off gui chrome
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+
+;; turn off startup screen
 (setq inhibit-startup-screen t)
+
+;; load theme
+(load-theme 'gruvbox t)
 
 (provide 'init-ui)
