06afa6c70de69f72564a8b1983e35b01d27a8e6b
Robert Malko <rmalko@doximity.com>
2017-09-30 12:28:35 -0700

    initial commit

---
+++ init.el
@@ -0,0 +1,36 @@
+; package management
+(require 'package)
+
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
+
+(setq package-enable-at-startup nil)
+(package-initialize)
+
+; add config files into load-path
+(add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
+
+; custom
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file 'noerror)
+
+; use package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+; init
+(require 'init-ui)
+
+; turn off backups
+(defvar backup-dir "~/.emacs.d/backups/")
+(setq backup-directory-alist (list (cons "." backup-dir)))
+(setq make-backup-files nil)
+
+; evil mode
+(require 'evil)
+(evil-mode t)
